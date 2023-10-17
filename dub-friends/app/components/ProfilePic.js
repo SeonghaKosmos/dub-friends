404340a3896a1f3555fc273f577082f5081702e2
Seongha Yi <seongha003@gmail.com>
2023-10-17 10:31:03 -0700

    simplified folder structure

--- dub-friends/app/components/ProfilePic.js
+++
@@ -1,6 +0,0 @@
-import './profile-pic.css'
-export default function ProfilePic(props) {
-    return(
-        <img src={props.src} className={`profile-pic ${props.styleClass}`}/>
-    )
-}
--- dub-friends/app/profile/TagBox.js
+++
@@ -1,9 +0,0 @@
-import './tag-box.css'
-
-export default function TagBox(props){
-    return(
-        <div className='tag-box'>
-            {props.children}
-        </div>
-    )
-}
